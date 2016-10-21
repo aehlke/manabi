18cc8eba3aab9ed7ab82ba1bec2c0d7bfe9616de
Alex Ehlke <alex.ehlke@gmail.com>
2016-10-20 21:47:01 -0400

    for screenshots...

--- manabi/apps/flashcards/models/review_availabilities.py
+++ manabi/apps/flashcards/models/review_availabilities.py
@@ -162,6 +162,9 @@ class ReviewAvailabilities(object):
                 u"",
             )
 
+        return ("You'll soon forget 19 expressions—now's a good time to review them.",
+            "We have 7 more you might have forgotten, plus new ones to learn.")
+
         return (
             u"This text will tell you about the cards ready for you to learn or review.",
             u"I haven't built this part of the backend API yet—this is beta, it'll come before release!"
