54cedcec36dadf2267b34806f8c2bb855281e538
Alex Ehlke <alex.ehlke@gmail.com>
2016-10-30 19:06:52 -0400

    forgot to hit save -_-

--- manabi/settings_production.py
+++ manabi/settings_production.py
@@ -22,7 +22,3 @@ SPARKPOST_OPTIONS = {
     'transactional': True,
 }
 
-WEBPACK_LOADER.update({
-    'BUNDLE_DIR_NAME': 'dist/',
-    'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats-prod.json')
-})
