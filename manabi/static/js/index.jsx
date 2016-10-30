62c8837bea90f1c82547bda453f18d7df57790ff
Alex Ehlke <alex.ehlke@gmail.com>
2016-10-30 16:25:13 -0400

    working single-line editor

--- manabi/static/js/index.jsx
+++ manabi/static/js/index.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Editor from 'draft-js-plugins-editor';
+import { EditorState } from 'draft-js'
+
+import createSingleLinePlugin from 'draft-js-single-line-plugin'
+const singleLinePlugin = createSingleLinePlugin()
+
+const plugins = [
+    singleLinePlugin,
+]
+
 
 class AnnotatedJapaneseInput extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {editorState: EditorState.createEmpty()};
-        this.onChange = (editorState) => this.setState({editorState});
+        super(props)
+
+        this.state = {editorState: EditorState.createEmpty()}
+        this.onChange = (editorState) => this.setState({editorState})
     }
     render() {
         const {editorState} = this.state;
-        return <Editor editorState={editorState} onChange={this.onChange} />;
+        return <Editor
+            editorState={editorState}
+            onChange={this.onChange}
+            plugins={plugins}
+        />
     }
 }
 
