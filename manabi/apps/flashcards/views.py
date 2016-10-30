677aca5785ef7339e3d3e630fb33a9f61f44fa18
Alex Ehlke <alex.ehlke@gmail.com>
2016-10-30 11:23:26 -0400

    wip card creation

--- manabi/apps/flashcards/views.py
+++ manabi/apps/flashcards/views.py
@@ -1,2 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+
+@login_required
+def card_creator(request):
+    return render(request, 'homepage.html')
