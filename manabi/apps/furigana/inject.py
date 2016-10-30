dbb8d95736593dfa604bf73f2bad487c65c4b202
Alex Ehlke <alex.ehlke@gmail.com>
2016-10-30 18:47:34 -0400

    finish furigana injection api

--- manabi/apps/furigana/inject.py
+++ manabi/apps/furigana/inject.py
@@ -1,28 +1,62 @@
 # -*- coding: utf-8 -*-
 
-from django.conf import settings
 import MeCab
+import jcconv
+from django.conf import settings
 
-tagger = MeCab.Tagger(settings.MECAB_RC_PATH)
+tagger = MeCab.Tagger('-Ochasen') #.format(settings.MECAB_RC_PATH))
 
 
 def _reading(node):
-    node.surface.decode('utf8').split(',')[-2]
+    surface = node.surface.decode('utf8')
+    reading = node.feature.decode('utf8').split(',')[-2]
+    if reading == '*':
+        return None
+    reading = jcconv.kata2hira(reading)
+
+    return reading
 
 
 def inject_furigana(text):
     injected_text = []
+    remaining_text = text
+
+    # https://runble1.com/python-mecab-morphological-analysis/
+    tagger.parse('')
 
     node = tagger.parseToNode(text.encode('utf8'))
     while node:
         surface = node.surface.decode('utf8')
+
+        # Add any skipped text.
+        node_index_in_remaining_text = remaining_text.find(surface)
+        injected_text.append(remaining_text[:node_index_in_remaining_text])
+        remaining_text = remaining_text[node_index_in_remaining_text + len(surface):]
+
         reading = _reading(node)
 
-        if reading == node.surface:
+        if (reading is None
+                or reading == surface
+                or jcconv.hira2kata(reading) == surface):
             injected_text.append(surface)
-        else:
-            injected_text.append(u'｜{}《{}》'.format(surface, reading))
+            node = node.next
+            continue
+
+        suffix = ''
+        redundant_length = 0
+        for surface_char, reading_char in zip(reversed(surface), reversed(reading)):
+            if surface_char != reading_char:
+                break
+            redundant_length += 1
+        if redundant_length > 0:
+            reading = reading[:-redundant_length]
+            suffix = surface[-redundant_length:]
+            surface = surface[:-redundant_length]
+
+        injected_text.append(u'｜{}《{}》{}'.format(surface, reading, suffix))
 
         node = node.next
+
+    injected_text.append(remaining_text)
 
     return u''.join(injected_text)
--- manabi/apps/furigana/tests.py
+++ manabi/apps/furigana/tests.py
@@ -3,6 +3,7 @@
 from manabi.test_helpers import (
     ManabiTestCase,
 )
+from manabi.apps.furigana.inject import inject_furigana
 
 
 class FuriganaInjectionAPITest(ManabiTestCase):
@@ -13,6 +14,18 @@ class FuriganaInjectionAPITest(ManabiTestCase):
 
     def test_expression_with_kanji(self):
         text_with_furigana = self.api.inject_furigana(u"背を寄せる")
-        print "Injected: ", text_with_furigana
-        print text_with_furigana
-        #  self.assertEqual(u"｜背《せ》を｜寄《よ》せる", text_with_furigana)
+        self.assertEqual(u"｜背《せ》を｜寄《よ》せる", text_with_furigana)
+
+
+class FuriganaInjectionTest(ManabiTestCase):
+    def test_plain_ascii_text(self):
+        ascii_text = u'foo bar'
+        text_with_furigana = inject_furigana(ascii_text)
+        self.assertEqual(ascii_text, text_with_furigana)
+
+    def test_expression_with_kanji(self):
+        text_with_furigana = inject_furigana(u"背を寄せる")
+        import sys
+        print >>sys.stderr,"Injected: ", text_with_furigana
+        print >>sys.stderr, u"｜背《せ》を｜寄《よ》せる"
+        self.assertEqual(u"｜背《せ》を｜寄《よ》せる", text_with_furigana)
