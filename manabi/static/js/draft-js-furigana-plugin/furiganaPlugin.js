8fce878aab3190f6f033b3459fff8c283dff8dc7
Alex Ehlke <alex.ehlke@gmail.com>
2016-11-04 21:59:47 -0400

    checkpoint before converting index.jsx furigana stuff to plugin

--- manabi/static/js/draft-js-furigana-plugin/furiganaPlugin.js
+++ manabi/static/js/draft-js-furigana-plugin/furiganaPlugin.js
@@ -1,6 +1,6 @@
 import decorateComponentWithProps from 'decorate-component-with-props';
 import { Map } from 'immutable';
-//import Mention from './Mention';
+import TextWithFurigana from './Furigana';
 //import MentionSuggestions from './MentionSuggestions';
 //import MentionSuggestionsPortal from './MentionSuggestionsPortal';
 // import defaultRegExp from './defaultRegExp';
@@ -13,39 +13,39 @@ import { Map } from 'immutable';
 // import defaultPositionSuggestions from './utils/positionSuggestions';
 
 const createFuriganaPlugin = (config = {}) => {
-  const defaultTheme = {
-    /*// CSS class for mention text
+    const defaultTheme = {
+        /*// CSS class for mention text
     mention: mentionStyles.mention,
-    // CSS class for suggestions component
+        // CSS class for suggestions component
     mentionSuggestions: mentionSuggestionsStyles.mentionSuggestions,
-    // CSS classes for an entry in the suggestions component
+        // CSS classes for an entry in the suggestions component
     mentionSuggestionsEntry: mentionSuggestionsEntryStyles.mentionSuggestionsEntry,
     mentionSuggestionsEntryFocused: mentionSuggestionsEntryStyles.mentionSuggestionsEntryFocused,
     mentionSuggestionsEntryText: mentionSuggestionsEntryStyles.mentionSuggestionsEntryText,
     mentionSuggestionsEntryAvatar: mentionSuggestionsEntryStyles.mentionSuggestionsEntryAvatar,*/
-  };
+    };
 
-  const callbacks = {
-    keyBindingFn: undefined,
-    handleKeyCommand: undefined,
-    onDownArrow: undefined,
-    onUpArrow: undefined,
-    onTab: undefined,
-    onEscape: undefined,
-    handleReturn: undefined,
-    onChange: undefined,
-  };
+    const callbacks = {
+        keyBindingFn: undefined,
+        handleKeyCommand: undefined,
+        onDownArrow: undefined,
+        onUpArrow: undefined,
+        onTab: undefined,
+        onEscape: undefined,
+        handleReturn: undefined,
+        onChange: undefined,
+    };
 
-  let searches = Map();
-  let escapedSearch;
-  let clientRectFunctions = Map();
+    let searches = Map();
+    let escapedSearch;
+    let clientRectFunctions = Map();
 
-  const store = {
-    getEditorState: undefined,
-    setEditorState: undefined,
-    getPortalClientRect: (offsetKey) => clientRectFunctions.get(offsetKey)(),
-    getAllSearches: () => searches,
-    /*isEscaped: (offsetKey) => escapedSearch === offsetKey,
+    const store = {
+        getEditorState: undefined,
+        setEditorState: undefined,
+        getPortalClientRect: (offsetKey) => clientRectFunctions.get(offsetKey)(),
+        getAllSearches: () => searches,
+        /*isEscaped: (offsetKey) => escapedSearch === offsetKey,
     escapeSearch: (offsetKey) => {
       escapedSearch = offsetKey;
     },
@@ -54,11 +54,11 @@ const createFuriganaPlugin = (config = {}) => {
       escapedSearch = undefined;
     },*/
 
-    /*register: (offsetKey) => {
+        /*register: (offsetKey) => {
       searches = searches.set(offsetKey, offsetKey);
     },*/
 
-    /*updatePortalClientRect: (offsetKey, func) => {
+        /*updatePortalClientRect: (offsetKey, func) => {
       clientRectFunctions = clientRectFunctions.set(offsetKey, func);
     },
 
@@ -66,58 +66,52 @@ const createFuriganaPlugin = (config = {}) => {
       searches = searches.delete(offsetKey);
       clientRectFunctions = clientRectFunctions.delete(offsetKey);
     },*/
-  };
+    };
 
-  // Styles are overwritten instead of merged as merging causes a lot of confusion.
-  //
-  // Why? Because when merging a developer needs to know all of the underlying
-  // styles which needs a deep dive into the code. Merging also makes it prone to
-  // errors when upgrading as basically every styling change would become a major
-  // breaking change. 1px of an increased padding can break a whole layout.
-  const {
-    theme = defaultTheme,
-    // positionSuggestions = defaultPositionSuggestions,
-    // mentionComponent,
-    // mentionTrigger = '@',
-    // mentionRegExp = defaultRegExp,
-  } = config;
-  const mentionSearchProps = {
-    callbacks,
-    theme,
-    store,
-    entityMutability: config.entityMutability ? config.entityMutability : 'SEGMENTED',
-    // positionSuggestions,
-    // mentionTrigger,
-  };
-  return {
-    //MentionSuggestions: decorateComponentWithProps(MentionSuggestions, mentionSearchProps),
-    decorators: [
-        // TODO: Decorate furigana entities with ruby/rt tags.
-      /*{
-        strategy: mentionStrategy(mentionTrigger),
-        component: decorateComponentWithProps(Mention, { theme, mentionComponent }),
-      },
-      {
-        strategy: mentionSuggestionsStrategy(mentionTrigger, mentionRegExp),
-        component: decorateComponentWithProps(MentionSuggestionsPortal, { store }),
-      },*/
-    ],
+    // Styles are overwritten instead of merged as merging causes a lot of confusion.
+    //
+    // Why? Because when merging a developer needs to know all of the underlying
+    // styles which needs a deep dive into the code. Merging also makes it prone to
+    // errors when upgrading as basically every styling change would become a major
+    // breaking change. 1px of an increased padding can break a whole layout.
+    const {
+        theme = defaultTheme,
+    } = config;
+    const mentionSearchProps = {
+        callbacks,
+        theme,
+        store,
+        entityMutability: config.entityMutability ? config.entityMutability : 'SEGMENTED',
+        // positionSuggestions,
+        // mentionTrigger,
+    };
+    return {
+        //MentionSuggestions: decorateComponentWithProps(MentionSuggestions, mentionSearchProps),
+        decorators: [
+            {
+                strategy: furiganaStrategy(),
+                component: decorateComponentWithProps(TextWithFurigana, { theme }),
+            },
+        ],
 
-    initialize: ({ getEditorState, setEditorState }) => {
-      store.getEditorState = getEditorState;
-      store.setEditorState = setEditorState;
-    },
+        initialize: ({ getEditorState, setEditorState }) => {
+            store.getEditorState = getEditorState;
+            store.setEditorState = setEditorState;
+        },
 
-    /*onDownArrow: (keyboardEvent) => callbacks.onDownArrow && callbacks.onDownArrow(keyboardEvent),
+        /*onDownArrow: (keyboardEvent) => callbacks.onDownArrow && callbacks.onDownArrow(keyboardEvent),
     onTab: (keyboardEvent) => callbacks.onTab && callbacks.onTab(keyboardEvent),
     onUpArrow: (keyboardEvent) => callbacks.onUpArrow && callbacks.onUpArrow(keyboardEvent),
     onEscape: (keyboardEvent) => callbacks.onEscape && callbacks.onEscape(keyboardEvent),
     handleReturn: (keyboardEvent) => callbacks.handleReturn && callbacks.handleReturn(keyboardEvent),*/
-    onChange: (editorState) => {
-      if (callbacks.onChange) return callbacks.onChange(editorState);
-      return editorState;
-    },
-  };
+        onChange: (editorState) => {
+            if (callbacks.onChange) return callbacks.onChange(editorState);
+
+            this.updateFurigana(editorState)
+
+            return editorState;
+        },
+    };
 };
 
 export default createFuriganaPlugin;
