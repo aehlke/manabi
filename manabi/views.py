c3b17fc754608f214f84f991cad03052883454e5
Alex Ehlke <alex.ehlke@gmail.com>
2016-11-24 22:53:47 -0500

    add bootstrap

--- manabi/views.py
+++ manabi/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from manabi.apps.flashcards.models import Deck
+
 
 def homepage(request):
     if request.user.is_anonymous():
         return render(request, 'landing_page.html')
 
-    return render(request, 'homepage.html')
+    decks = Deck.objects.of_user(request.user)
+
+    return render(request, 'homepage.html', {
+        'decks': decks,
+    })
