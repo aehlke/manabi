85cf749b7d15dad62a418d24b8337b9517aae4e1
Alex Ehlke <alex.ehlke@gmail.com>
2016-12-05 15:18:29 -0500

    add branch link from web

--- manabi/context_processors.py
+++ manabi/context_processors.py
@@ -5,4 +5,5 @@ def url_prefixes(request):
     return {
         'DEFAULT_URL_PREFIX': settings.DEFAULT_URL_PREFIX,
         'API_URL_PREFIX': settings.API_URL_PREFIX,
+        'BRANCH_KEY': settings.BRANCH_KEY,
     }
