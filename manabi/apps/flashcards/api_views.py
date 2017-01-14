40d8515abd7ef299bc8963c0f51c4dbd823d7254
Alex Ehlke <alex.ehlke@gmail.com>
2017-01-14 11:06:05 -0500

    missing import

--- manabi/apps/flashcards/api_views.py
+++ manabi/apps/flashcards/api_views.py
@@ -5,7 +5,10 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.decorators import detail_route, list_route
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+)
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.serializers import ValidationError
