65768f5fc9cc248faba17885523cab89f9d996e6
Alex Ehlke <alex.ehlke@gmail.com>
2017-01-23 22:53:02 -0500

    unneeded in django 1.9

--- manabi/apps/featured_decks/admin.py
+++ manabi/apps/featured_decks/admin.py
@@ -10,18 +10,4 @@ from manabi.apps.flashcards.models import Deck
 class FeaturedDeckAdmin(SortableAdmin):
     model = FeaturedDeck
 
-    list_display = ['deck', 'link_to_deck_admin']
-
-    def link_to_deck_admin(self, obj):
-        return "WHAT"
-        url = urlresolvers.reverse(
-            'admin:manabi_deck_change', args=[obj.Deck.id])
-        return u'<a href="{}">{}</a>'.format(url, obj.Deck.name)
-    link_to_deck_admin.allow_tags = True
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'deck':
-            kwargs['queryset'] = Deck.objects.shared_decks()
-
-        return super(FeaturedDeckAdmin, self).formfield_for_foreignkey(
-            db_field, request, **kwargs)
+    list_display = ['deck']
