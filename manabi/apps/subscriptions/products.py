9c2afc8f68df04dc0d23b91695c489dd7c67398b
Alex Ehlke <alex.ehlke@gmail.com>
2017-02-26 11:09:55 -0500

    copy tweak

--- manabi/apps/subscriptions/products.py
+++ manabi/apps/subscriptions/products.py
@@ -1,7 +1,7 @@
 _NON_BREAKING_SPACE = unichr(160)
 PRIMARY_PROMPT = "Manabi adapts to how you learn, tracking your strength of memory to determine the optimal time for you to review right before you forget.\n\nSubscribe to unlock unlimited" + _NON_BREAKING_SPACE + "personalized" + _NON_BREAKING_SPACE + "reviews."
 
-STUDENT_PROMPT = "Are you a student, educator, or parent of a student? Manabi is available to you at a discount."
+STUDENT_PROMPT = "Are you a student, educator, or parent? Manabi is available to you at a discount."
 
 
 def _product(product_id_suffix, subtitle):
