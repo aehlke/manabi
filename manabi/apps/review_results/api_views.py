35047907a1930a54d76d6c62e33d931666bf5680
Alex Ehlke <alex.ehlke@gmail.com>
2017-11-03 22:29:12 -0400

    missing import

--- manabi/apps/review_results/api_views.py
+++ manabi/apps/review_results/api_views.py
@@ -4,6 +4,7 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import (
     IsAuthenticated,
 )
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from manabi.apps.review_results.models import (
