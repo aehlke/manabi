9e0b862910ea80b0ad4b85ea1c990c40c8085ad8
Alex Ehlke <alex.ehlke@gmail.com>
2017-12-13 17:43:54 -0500

    thumbnail is optional

--- manabi/apps/reader_sources/models.py
+++ manabi/apps/reader_sources/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class ReaderSource(models.Model):
     source_url = models.URLField(max_length=700, unique=True)
     title = models.CharField(max_length=500)
-    thumbnail_url = models.URLField(max_length=700)
+    thumbnail_url = models.URLField(max_length=700, blank=True, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     modified_at = models.DateTimeField(blank=True, null=True)
