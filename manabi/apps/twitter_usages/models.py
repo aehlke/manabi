e707d9f2ba8668c6afc0ad035deb11099b477fa2
Alex Ehlke <alex.ehlke@gmail.com>
2018-02-27 17:16:46 -0500

    unicode

--- manabi/apps/twitter_usages/models.py
+++ manabi/apps/twitter_usages/models.py
@@ -16,11 +16,11 @@ def search_expressions(fact):
     Returns list of strings that should be used to search for usages for the
     given fact.
     '''
-    suru = '（する）'
+    suru = u'（する）'
     if suru in fact.expression:
         return [
             fact.expression.replace(suru, ''),
-            fact.expression.replace(suru, 'する'),
+            fact.expression.replace(suru, u'する'),
         ]
 
     return [fact.expression]
