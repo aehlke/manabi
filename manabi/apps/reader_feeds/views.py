ff66e15876ce9f161493bc964901a6da138f9b56
Alex Ehlke <alex.ehlke@gmail.com>
2018-03-16 08:52:43 -0400

    cache feed

--- manabi/apps/reader_feeds/views.py
+++ manabi/apps/reader_feeds/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page, cache_control
 
 from manabi.apps.reader_feeds.nhk_easy_news import (
     generate_nhk_easy_news_feed,
 )
 
 
+FEED_MAX_AGE = 60 * 60 * 4
+
+
+@cache_page(60 * 60)
+@cache_control(max_age=FEED_MAX_AGE)
 def nhk_easy_news(request):
     feed_content = generate_nhk_easy_news_feed()
     return HttpResponse(
