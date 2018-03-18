8d8c1094f1eab3b9f505ad55f8d9b4b5c82ac60a
Alex Ehlke <alex.ehlke@gmail.com>
2018-03-18 14:43:50 -0400

    syntax

--- manabi/apps/reader_feeds/nhk_easy_news.py
+++ manabi/apps/reader_feeds/nhk_easy_news.py
@@ -58,7 +58,7 @@ def _clean_content(content):
     for _ in range(2):
         content_tree.remove(content_tree[-1])
 
-    return u'<article class="article"">{}</article>'.format(lxml.html.tostring(
+    return u'<article class="article">{}</article>'.format(lxml.html.tostring(
         content_tree, pretty_print=True, encoding='unicode'))
 
 
