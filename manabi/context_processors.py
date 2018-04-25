ffd54d569f62c84b29130fd799f945e7f3533170
Alex Ehlke <alex.ehlke@gmail.com>
2018-04-25 08:46:32 -0400

    API_URL_PREFIX is unused

--- manabi/context_processors.py
+++ manabi/context_processors.py
@@ -4,6 +4,5 @@ from django.conf import settings
 def url_prefixes(request):
     return {
         'DEFAULT_URL_PREFIX': settings.DEFAULT_URL_PREFIX,
-        'API_URL_PREFIX': settings.API_URL_PREFIX,
         'BRANCH_KEY': settings.BRANCH_KEY,
     }
