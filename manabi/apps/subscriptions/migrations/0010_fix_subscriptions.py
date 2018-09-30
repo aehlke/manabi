50f9635c5e09a71f3781281bf53b3e7cae5ab4ac
Alex Ehlke <alex.ehlke@gmail.com>
2018-09-29 21:48:52 -0400

    fix correct migration

--- manabi/apps/subscriptions/migrations/0010_fix_subscriptions.py
+++ manabi/apps/subscriptions/migrations/0010_fix_subscriptions.py
@@ -4,9 +4,9 @@ from django.db import migrations
 
 
 def forwards(apps, schema_editor):
+    from manabi.apps.subscriptions.models import InAppPurchaseLogItem
+
     Subscription = apps.get_model('subscriptions', 'Subscription')
-    InAppPurchaseLogItem = apps.get_model(
-        'subscriptions', 'InAppPurchaseLogItem')
 
     for log_item in InAppPurchaseLogItem.objects.all():
         if not log_item.original_transaction_id:
