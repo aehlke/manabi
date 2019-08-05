8736ab328346615b8d88a3c27610609a5e75c6e4
Alex Ehlke <alex.ehlke@gmail.com>
2019-08-05 17:56:11 -0400

    remove old test

--- manabi/apps/manabi_auth/tests.py
+++ manabi/apps/manabi_auth/tests.py
@@ -1,8 +1,1 @@
 from manabi.test_helpers import ManabiTestCase
-
-
-class RegistrationAPITest(ManabiTestCase):
-    def test_successful_registration_returns_a_token(self):
-        resp = self.api.register('myname', 'mypassword')
-        token = resp['auth_token']
-        self.assertTrue(token)
