b315b4ca638ee8242db6d5cbc4f9914e1c8e761f
Alex Ehlke <alex.ehlke@gmail.com>
2019-12-24 15:12:33 -0500

    add wired

---
+++ manabi/apps/reader_feeds/management/commands/generate_wired.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+import asyncio
+import subprocess
+import tempfile
+
+from django.core.files.storage import default_storage
+from django.core.management.base import BaseCommand, CommandError
+
+from manabi.apps.reader_feeds.feed_storage import save_feed
+from manabi.apps.reader_feeds.wired import generate_feed
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('--dev', action='store_true')
+        parser.add_argument('--entry-limit', type=int, default=None)
+
+    async def run(self, *args, **options):
+        feed_kwargs = {}
+        if options['entry_limit'] is not None:
+            feed_kwargs['entry_limit'] = options['entry_limit']
+        if options['dev']:
+            feed_kwargs['return_content_only'] = True
+
+        xml = await generate_feed(**feed_kwargs)
+
+        if options['dev']:
+            with tempfile.NamedTemporaryFile(
+                delete=False, suffix='.html',
+            ) as tf:
+                tf.write(f'<html><body>{xml}</body></html>'.encode('utf-8'))
+            subprocess.call(['open', tf.name])
+        else:
+            save_feed(f'nhk-news.rss', xml)
+
+    def handle(self, *args, **options):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.run(*args, **options))
+        loop.close()
---
+++ manabi/apps/reader_feeds/wired.py
@@ -0,0 +1,88 @@
+# -*- encoding: utf-8 -*-
+import asyncio
+import re
+from datetime import datetime
+from time import mktime
+from urllib.parse import urlparse, urlunparse, urljoin
+
+import lxml.html
+import feedparser
+import praw
+import pytz
+import requests
+from django.conf import settings
+from feedgen.feed import FeedGenerator
+from lxml.cssselect import CSSSelector
+from lxml import etree
+from requests_html import AsyncHTMLSession
+
+
+async def _get_image_url(article_url):
+    session = AsyncHTMLSession()
+    r = await session.get(article_url, timeout=20)
+    doc = r.html.lxml
+    return doc.cssselect('meta[property="og:image"]')[0].get('content')
+
+
+async def _process_and_add_entry(post, fg):
+    entry = fg.add_entry()
+
+    title = post.title
+    entry.title(title)
+
+    published_struct = post.published_parsed
+    entry.published(
+        datetime.fromtimestamp(mktime(published_struct))
+        .replace(tzinfo=pytz.utc))
+
+    article_url = post.link
+    entry.id(article_url)
+    entry.link({'href': article_url})
+
+    image_url = await _get_image_url(article_url)
+    if image_url:
+        entry.link(href=image_url, rel='enclosure', type='image/jpeg')
+
+    description = post.summary
+    entry.description(description)
+
+    return entry
+
+
+async def generate_feed(
+    entry_limit=None,
+    return_content_only=False,
+):
+    fg = FeedGenerator()
+    fg.id('https://wired.jp/')
+    fg.title('WIRED.jp')
+    fg.language('ja')
+
+    feed = feedparser.parse('https://wired.jp/rssfeeder/')
+
+    entries = []
+    for post in reversed(feed.entries):
+        entry = await _process_and_add_entry(post, fg)
+
+        if entry is not None:
+            entries.append(entry)
+
+        if entry is not None and entry_limit is not None:
+            entry_limit -= 1
+
+        if entry_limit <= 0:
+            break
+
+    if return_content_only:
+        html = ''
+        for entry in reversed(entries):
+            title = entry.title()
+            content = entry.content()['content']
+            html += f'<h1>{title}</h1>{content}'
+        return html
+
+    if fg.entry() == 0:
+        raise Exception("Generated zero feed entries.")
+
+    return fg.atom_str(pretty=True, encoding='utf-8')
+
